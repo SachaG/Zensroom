c2bfacb07571270b88f9128f994a7456e5a9be0e
therealedsheenan <private.sheenantenepre@gmail.com>
2017-08-23 22:39:50 +0900

    booking dashboard init

--- packages/zensroom/lib/components/admin/BookingsDashboard.js
+++ packages/zensroom/lib/components/admin/BookingsDashboard.js
@@ -13,17 +13,35 @@ import { FormattedMessage } from 'meteor/vulcan:i18n';
 
 import Bookings from '../../modules/bookings/collection.js';
 
+const PriceComponent = (props) => {
+  console.log(props)
+  return (
+    <div>this is a test</div>
+  )
+}
+
+const columns = [
+  'startAt',
+  'endAt',
+  'numberOfGuests',
+  'paidAt',
+  {
+    name: 'Price',
+    component: PriceComponent
+  }
+]
+
 const BookingsDashboard = () =>
 
   <div className="bookings-dashboard">
-    
+
     <h3><FormattedMessage id='bookings.bookings'/></h3>
 
-    <Components.Datatable 
-      collection={Bookings} 
-      columns={['startAt', 'endAt']} 
+    <Components.Datatable
+      collection={Bookings}
+      columns={columns}
     />
-  
+
   </div>
 
 registerComponent('BookingsDashboard', BookingsDashboard);
