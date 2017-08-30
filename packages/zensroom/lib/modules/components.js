4a846d9afcd5e73f5dc8734da0323879556666fc
therealedsheenan <private.sheenantenepre@gmail.com>
2017-08-30 19:18:46 +0900

    routes

--- packages/zensroom/lib/modules/components.js
+++ packages/zensroom/lib/modules/components.js
@@ -17,6 +17,7 @@ import '../components/bookings/BookingsNewForm';
 import '../components/bookings/BookingsPage';
 import '../components/bookings/BookingsRoomUser';
 import '../components/bookings/BookingsPending';
+import '../components/bookings/BookingCompleted';
 
 import '../components/common/Footer';
 import '../components/common/Header';
--- packages/zensroom/lib/modules/routes.js
+++ packages/zensroom/lib/modules/routes.js
@@ -33,21 +33,22 @@ addRoute([
   {name: 'about',                 path: '/about',                   componentName: 'About'},
   {name: 'privacy',               path: '/privacy',                 componentName: 'Privacy'},
   {name: 'terms',                 path: '/terms',                   componentName: 'Terms'},
-          
+
   {name: 'rooms.search',          path: '/search',                  componentName: 'RoomsSearch'},
   {name: 'rooms.new',             path: '/room/new',                componentName: 'RoomsNewPage'},
   {name: 'rooms.page',            path: '/room/:roomId(/:slug)',    componentName: 'RoomsPage'},
-          
+
   {name: 'bookings.page',         path: '/booking/:bookingId',      componentName: 'BookingsPage'},
-          
+  {name: 'bookings.completed',         path: '/booking/:bookingId/completed',      componentName: 'BookingCompleted'},
+
   {name: 'users.single',          path:'/users/:slug',              componentName: 'UsersProfile'},
   {name: 'users.account',         path:'/account',                  componentName: 'UsersAccount'},
   {name: 'users.edit',            path:'/users/:slug/edit',         componentName: 'UsersAccount'},
   {name: 'users.signup',          path:'/sign-up',                  componentName: 'UsersSignUp'},
   {name: 'users.login',           path:'/log-in',                   componentName: 'UsersLogIn'},
-    
+
   {name: 'bookings.dashboard',    path:'/admin/bookings',           componentName: 'BookingsDashboard',   layoutName: 'AdminLayout'},
   {name: 'rooms.dashboard',       path:'/admin/rooms',              componentName: 'RoomsDashboard',      layoutName: 'AdminLayout'},
   {name: 'reviews.dashboard',     path:'/admin/reviews',            componentName: 'ReviewsDashboard',    layoutName: 'AdminLayout'},
-  
+
 ]);
