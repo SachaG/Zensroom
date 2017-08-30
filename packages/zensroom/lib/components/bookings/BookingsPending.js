31bb430ef68ee9b55871d2299819d9b5f9a604f3
therealedsheenan <private.sheenantenepre@gmail.com>
2017-08-30 19:14:22 +0900

    add alert react-bootsrap

--- packages/zensroom/lib/components/bookings/BookingsPending.js
+++ packages/zensroom/lib/components/bookings/BookingsPending.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import { Components, registerComponent, withList, withCurrentUser, withDocument } from 'meteor/vulcan:core';
 import { Link } from 'react-router';
+import { Alert } from 'react-bootstrap';
 import compose from 'recompose/compose';
 
 import Bookings from '../../modules/bookings/collection.js';
 
 const BookingsPending = ({loading, results}) => {
   return (
-    <div>
+    <Alert bsStyle="danger">
       {loading ? <Components.Loading/> :
         <div>
-        {results.map((booking) => {
-          if (booking.paidAt === null) {
-            return (
-              <div key={booking._id}>
-                <Link to={`/booking/${booking._id}`}>Complete your booking.</Link>
-              </div>
-            )
-          }
-        })}
+        {results.map((booking) => (
+          <div key={booking._id}>
+            <Link to={`/booking/${booking._id}`}>Complete your booking.</Link>
+          </div>
+        ))}
         </div>
       }
-    </div>
+    </Alert>
   )
-}
+};
 
 const options = {
   collection: Bookings
-}
+};
 
 registerComponent('BookingsPending', BookingsPending, [withList, options]);
 
