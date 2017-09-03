a9a3d269c97a62c002568d79ac6f4ad14b3a300e
therealedsheenan <private.sheenantenepre@gmail.com>
2017-09-03 16:10:52 +0900

    user menu

--- packages/zensroom/lib/components/users/UsersMenu.js
+++ packages/zensroom/lib/components/users/UsersMenu.js
@@ -30,31 +30,35 @@ const UsersMenu = ({currentUser, client}) =>
           <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="users.edit_account"/></MenuItem>
         </LinkContainer>
 
-        {Users.canDo(currentUser, 'rooms.new') ? 
+        <LinkContainer to={`/accounts/bookings/past`}>
+          <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="bookings.past"/></MenuItem>
+        </LinkContainer>
+
+        {Users.canDo(currentUser, 'rooms.new') ?
           <LinkContainer to={`/room/new`}>
             <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="rooms.create_new"/></MenuItem>
           </LinkContainer>
         : null}
 
-        {Users.canDo(currentUser, 'users.view.all') ? 
+        {Users.canDo(currentUser, 'users.view.all') ?
           <LinkContainer to={`/admin/`}>
             <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="users.users_admin"/></MenuItem>
           </LinkContainer>
         : null}
 
-        {Users.canDo(currentUser, 'bookings.view.all') ? 
+        {Users.canDo(currentUser, 'bookings.view.all') ?
           <LinkContainer to={`/admin/bookings`}>
             <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="bookings.bookings_admin"/></MenuItem>
           </LinkContainer>
         : null}
 
-        {Users.canDo(currentUser, 'rooms.view.all') ? 
+        {Users.canDo(currentUser, 'rooms.view.all') ?
           <LinkContainer to={`/admin/rooms`}>
             <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="rooms.rooms_admin"/></MenuItem>
           </LinkContainer>
         : null}
 
-        {Users.canDo(currentUser, 'reviews.view.all') ? 
+        {Users.canDo(currentUser, 'reviews.view.all') ?
           <LinkContainer to={`/admin/reviews`}>
             <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="reviews.reviews_admin"/></MenuItem>
           </LinkContainer>
