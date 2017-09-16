6fb47f6188d6812001dd64863b1802ea0bf2ac0e
therealedsheenan <private.sheenantenepre@gmail.com>
2017-09-16 20:10:10 +0900

    accounts layout

--- packages/zensroom/lib/components/users/UsersBookings.js
+++ packages/zensroom/lib/components/users/UsersBookings.js
@@ -10,21 +10,17 @@ import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'meteor/vulcan:i18n';
 
-const UsersBookings = (props) => {
-  return (
-    <div>
-      <h3><FormattedMessage id="bookings.past" /></h3>
-      <Components.BookingsPast />
+const UsersBookings = () =>
+  <div>
+    <h3><FormattedMessage id="bookings.past" /></h3>
+    <Components.BookingsPast />
 
-      <h3><FormattedMessage id="bookings.current" /></h3>
-      <Components.BookingsCurrent />
+    <h3><FormattedMessage id="bookings.current" /></h3>
+    <Components.BookingsCurrent />
 
-      <h3><FormattedMessage id="bookings.future" /></h3>
-      <Components.BookingsFuture />
-    </div>
-  )
-};
-
+    <h3><FormattedMessage id="bookings.future" /></h3>
+    <Components.BookingsFuture />
+  </div>;
 
 UsersBookings.propTypes = {
   terms: PropTypes.object, // a user is defined by its unique _id or its unique slug
