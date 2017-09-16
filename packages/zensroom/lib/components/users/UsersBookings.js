b80d1e5e3904b9e22fd837e9dbf8a736fb850484
therealedsheenan <private.sheenantenepre@gmail.com>
2017-09-16 18:22:53 +0900

    user bookings label

--- packages/zensroom/lib/components/users/UsersBookings.js
+++ packages/zensroom/lib/components/users/UsersBookings.js
@@ -8,11 +8,19 @@ import { Components, registerComponent, withCurrentUser } from 'meteor/vulcan:co
 import React from 'react';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'meteor/vulcan:i18n';
 
 const UsersBookings = (props) => {
   return (
     <div>
-      <Components.RoomsSearchForm />
+      <h3><FormattedMessage id="bookings.past" /></h3>
+      <Components.BookingsPast />
+
+      <h3><FormattedMessage id="bookings.current" /></h3>
+      <Components.BookingsCurrent />
+
+      <h3><FormattedMessage id="bookings.future" /></h3>
+      <Components.BookingsFuture />
     </div>
   )
 };
